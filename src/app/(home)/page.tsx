8900a1a3cdcedf94b8281803eff3c3a90724b8f5
nayara-al <anna_nayara@hotmail.com>
2023-07-28 23:36:09 -0300

    criação da página nossa história

--- src/app/(home)/page.tsx
+++
@@ -1,8 +0,0 @@
-import GuestConfirm from '@/components/GuestConfirm'
-import React from 'react'
-
-export default function pages() {
-  return (
-    <GuestConfirm/>
-  )
-}
---
+++ src/app/nossa-historia/page.tsx
@@ -0,0 +1,19 @@
+import GuestConfirm from "@/components/GuestConfirm";
+import { NavBar } from "@/components/NavBar";
+import { PageTitle } from "@/components/PageTitle";
+import { Scrap } from "@/components/Scrap";
+import React from "react";
+
+export default function pages() {
+  return (
+    <div className="flex bg-bgPage h-full w-full">
+      <NavBar />
+      <div>
+        <PageTitle title="Nossa história"/>
+        <div className="flex self-center justify-center ">
+          <Scrap/>
+        </div>
+      </div>
+    </div>
+  );
+}
