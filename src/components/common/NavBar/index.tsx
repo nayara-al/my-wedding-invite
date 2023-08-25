f7d75b21f31b5f0d17375a2c5f030ec0e9cf8681
nayara-al <anna_nayara@hotmail.com>
2023-08-24 23:01:11 -0300

    feat: phosphor-icons

--- src/components/common/NavBar/index.tsx
+++ src/components/common/NavBar/index.tsx
@@ -1,10 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import menu from "../../../../public/menu.png";
-import home from "../../../../public/home.png";
-import story from "../../../../public/story.png";
+import { List, House, PencilLine, CalendarCheck } from "@phosphor-icons/react"
 import rings from "../../../../public/rings.png";
-import calendar from "../../../../public/calendar.png";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -41,13 +38,7 @@ export const NavBar = () => {
         onClick={toggleText}
         className="bg-secondGreen flex items-center gap-12 px-4 h-12"
       >
-        <Image
-          src={menu}
-          alt="menu hamburguer"
-          width={32}
-          height={32}
-          className="hover:cursor-pointer"
-        />
+        <List width={32} height={32} className="cursor-pointer"/>
         {showText && (
           <p className="hover:cursor-pointer font-gwendolyn text-[2rem]">
             Menu
@@ -55,15 +46,15 @@ export const NavBar = () => {
         )}
       </header>
       <ul className="flex flex-col gap-4 px-2">
-        <Link href={"/"}>
+        <Link href={"/"} className="text-center">
           <li className="flex gap-6 py-2">
-            <Image src={home} alt="ícone de casa" />
+            <House width={44} height={44} weight="light" />
             {showText && <p className="font-poppins self-center">Home</p>}
           </li>
         </Link>
         <Link href={"/nossa-historia"}>
           <li className="flex gap-6 py-2">
-            <Image src={story} alt="ícone de lápis para escrita" />
+            <PencilLine width={44} height={44} weight="light"/>
             {showText && (
               <p className="font-poppins self-center">Nossa história</p>
             )}
@@ -79,7 +70,7 @@ export const NavBar = () => {
         </Link>
         <Link href={"/salvar-data"}>
           <li className="flex w-full gap-6 py-2">
-            <Image src={calendar} alt="ícone de calendário" />
+            <CalendarCheck width={44} height={44} weight="light"/> 
             {showText && (
               <p className="font-poppins self-center">Salvar a data?</p>
             )}
