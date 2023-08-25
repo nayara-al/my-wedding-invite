485e109ffcc629ecd316fa5c7e045db06f02a5d1
nayara-al <anna_nayara@hotmail.com>
2023-08-24 23:12:59 -0300

    refactor: controlar e aumentar tamanho do controller

--- src/components/pageComponents/salvar-data/GameBoy/Direcional/index.tsx
+++ src/components/pageComponents/salvar-data/GameBoy/Direcional/index.tsx
@@ -1,17 +1,20 @@
-export const Directional = () => {
+interface DirectionalProps {
+  changeOption: () => void
+}
+export const Directional = ({changeOption}: DirectionalProps) => {
   return (
     <div className="grid grid-cols-3">
-      <div className="w-5 h-5"></div>
-      <div className="w-5 h-5 bg-darkGray"></div>
-      <div className="w-5 h-5"></div>
-      <div className="w-5 h-5 bg-darkGray"></div>
-      <div className="w-5 h-5 bg-darkGray">
-        <span className="w-3 h-3 rounded-full border-solid border-1 border-grayBase"></span>
+      <div className="w-7 h-7"></div>
+      <button className="w-7 h-7 bg-darkGray rounded-t-md" type="button" onClick={changeOption}></button>
+      <div className="w-7 h-7"></div>
+      <div className="w-7 h-7 bg-darkGray rounded-l-md cursor-not-allowed"></div>
+      <div className="w-7 h-7 bg-darkGray flex items-center justify-center">
+        <div className="w-5 h-5 rounded-full border-solid border-1 border-grayBase bg-slate-600"></div>
       </div>
-      <div className="w-5 h-5 bg-darkGray"></div>
-      <div className="w-5 h-5"></div>
-      <div className="w-5 h-5 bg-darkGray"></div>
-      <div className="w-5 h-5"></div>
+      <div className="w-7 h-7 bg-darkGray rounded-r-md cursor-not-allowed"></div>
+      <div className="w-7 h-7"></div>
+      <button className="w-7 h-7 bg-darkGray rounded-b-md" type="button" onClick={changeOption}></button>
+      <div className="w-7 h-7"></div>
     </div>
   );
 };
