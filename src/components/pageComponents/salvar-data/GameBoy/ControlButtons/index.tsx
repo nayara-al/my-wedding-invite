b4161bc5d49383169b8f7f7ab324d272d836ec57
nayara-al <anna_nayara@hotmail.com>
2023-08-24 23:15:18 -0300

    refactor: ajuste nos controladores

--- src/components/pageComponents/salvar-data/GameBoy/ControlButtons/index.tsx
+++ src/components/pageComponents/salvar-data/GameBoy/ControlButtons/index.tsx
@@ -1,17 +1,17 @@
 export const ControlButtons = () => {
   return (
-    <div className="grid grid-cols-2 gap-1 bg-darkGray w-20 h-8 rounded-lg rotate-[-30deg]">
+    <div className="grid grid-cols-2 gap-1 bg-darkGray w-24 h-12 rounded-lg rotate-[-30deg]">
       <div className="flex justify-center items-center relative">
         <button
           type="submit"
-          className="w-5 h-5 bg-aBButton rounded-xl"
+          className="w-7 h-7 bg-aBButton rounded-full"
         ></button>
         <span className="font-pressStart2P absolute top-[-20px] left-1/2 transform -translate-x-1/2 text-white text-xs font-bold">
           a
         </span>
       </div>
       <div className="flex justify-center items-center relative">
-        <div className="w-5 h-5 bg-aBButton rounded-xl"></div>
+        <div className="w-7 h-7 bg-aBButton rounded-full"></div>
         <span className="font-pressStart2P absolute top-[-20px] left-1/2 transform -translate-x-1/2 text-white text-xs font-bold">
           b
         </span>
