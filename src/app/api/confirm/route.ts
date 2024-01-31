3a6fb8b98e83079dc79f4601989e8f1d3111fda2
nayara-al <anna_nayara@hotmail.com>
2024-01-30 21:54:27 -0300

    refactor: ajuste de api

---
+++ src/app/api/confirm/route.ts
@@ -0,0 +1,43 @@
+import nodemailer from "nodemailer";
+
+export async function POST(request: Request) {
+  try {
+    const { firstName, email, message } = await request.json();
+    const transporter = nodemailer.createTransport({
+      service: process.env.MAIL_DOMAIN,
+      host: process.env.MAIL_HOST,
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.MAIL_SENDER,
+        pass: process.env.MAIL_SENDER_PASS,
+      },
+    });
+
+    const mailOption = {
+      from: process.env.MAIL_SENDER,
+      to: process.env.MAIL_SENDER,
+      subject: "Confirmação de presença",
+      html: `
+      <h3>Olá, temos uma nova confirmação de presença para o casamento!</h3>
+      <li>Nome: ${firstName} </li>
+      <li>E-mail: ${email} </li>
+      <li>Mensagem: ${message} </li>
+        `,
+    };
+    const response = await transporter.sendMail(mailOption);
+    if (response.accepted)
+      return Response.json({ message: "Email Sent Successfully" });
+    else
+      Response.json(
+        { message: "Algo deu errado, tente novamente mais tarde." },
+        { status: 500 }
+      );
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      { message: "Algo deu errado, tente novamente mais tarde." },
+      { status: 500 }
+    );
+  }
+}
