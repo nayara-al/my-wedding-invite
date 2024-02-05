37cdba79de6685e5d1514b62f39e27af37368ce3
nayara-al <anna_nayara@hotmail.com>
2024-02-04 22:07:44 -0300

    ajuste de largura

--- src/components/common/Marquee/index.tsx
+++ src/components/common/Marquee/index.tsx
@@ -3,8 +3,8 @@ import Marquee from "react-fast-marquee"
 
 export default function SectionMarquee() {
   return (
-    <div className="py-12 h-full bg-white">
-      <Marquee autoFill pauseOnHover pauseOnClick gradient gradientColor="#BDAFAA" gradientWidth="120">
+    <div className="py-12 h-full bg-white w-full">
+      <Marquee autoFill pauseOnHover pauseOnClick gradient gradientColor="#BDAFAA" gradientWidth="120" className="max-w-[800px]">
           <Image width={400} height={400} src={"/images/3.jpg"} alt="" className="mx-8 rounded-xl"/>
           <Image width={400} height={400} src={"/images/4.jpg"} alt="" className="mx-8 rounded-xl"/>
           <Image width={400} height={400} src={"/images/5.jpg"} alt="" className="mx-8 rounded-xl"/>
