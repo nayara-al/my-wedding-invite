3354abf426181c7a1cb5196d5a8f2b443db81643
Anna Nayara Lira <anna_nayara@hotmail.com>
2024-02-08 11:30:13 -0300

    refactor: mudança de cor dispositivo mobile

--- src/components/common/PageTitle/index.tsx
+++ src/components/common/PageTitle/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const PageTitle = () => {
   return (
-    <header className="md:hidden bg-second text-[32px] flex items-center justify-center p-2 text-white font-gwendolyn">
+    <header className="md:hidden bg-primary text-[32px] flex items-center justify-center p-2 text-white font-gwendolyn">
       <Image src="/coracao.svg" alt="icon de coração" width={40} height={40} />
     </header>
   );
