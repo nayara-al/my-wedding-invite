2db9680df9e92ad5143f5426f6293f37bff4195b
Anna Nayara Lira <anna_nayara@hotmail.com>
2024-02-08 14:13:33 -0300

    refactor: abrir imagens em nova aba

--- src/components/common/Marquee/index.tsx
+++ src/components/common/Marquee/index.tsx
@@ -22,7 +22,7 @@ export default function SectionMarquee() {
             className="mx-8 rounded-xl"
           />
           <p className="text-xs text-slate-400">
-            <a href="https://br.freepik.com/fotos-gratis/gente-alegria-momento-agradavel-na-vida-mulher-jovem-europeia-morena-de-alegria-em-pe-perto-de-seu-namorado-vai-cobrir-os-olhos-e-fazer-surpresa-se-divertir-juntos-isolados-sobre-uma-parede-branca_10421304.htm#page=3&position=23&from_view=author&uuid=334cc443-039f-48f7-91e4-6edb31124f30">
+            <a target="_blank" href="https://br.freepik.com/fotos-gratis/gente-alegria-momento-agradavel-na-vida-mulher-jovem-europeia-morena-de-alegria-em-pe-perto-de-seu-namorado-vai-cobrir-os-olhos-e-fazer-surpresa-se-divertir-juntos-isolados-sobre-uma-parede-branca_10421304.htm#page=3&position=23&from_view=author&uuid=334cc443-039f-48f7-91e4-6edb31124f30">
               Imagem de wayhomestudio
             </a>{" "}
             no Freepik
@@ -37,7 +37,7 @@ export default function SectionMarquee() {
             className="mx-8 rounded-xl"
           />
           <p className="text-xs text-slate-400">
-            <a href="https://br.freepik.com/fotos-gratis/foto-horizontal-de-amigos-felizes-apontando-o-dedo-para-voce-gesticule-para-dentro-faca-uma-escolha-tenha-expressoes-positivas_10749544.htm#page=2&position=20&from_view=author&uuid=334cc443-039f-48f7-91e4-6edb31124f30">
+            <a target="_blank" href="https://br.freepik.com/fotos-gratis/foto-horizontal-de-amigos-felizes-apontando-o-dedo-para-voce-gesticule-para-dentro-faca-uma-escolha-tenha-expressoes-positivas_10749544.htm#page=2&position=20&from_view=author&uuid=334cc443-039f-48f7-91e4-6edb31124f30">
               Imagem de wayhomestudio
             </a>{" "}
             no Freepik
@@ -52,7 +52,7 @@ export default function SectionMarquee() {
             className="mx-8 rounded-xl"
           />
           <p className="text-xs text-slate-400">
-            <a href="https://br.freepik.com/fotos-gratis/casal-adoravel-tem-um-abraco-caloroso_13759431.htm#query=casal&position=0&from_view=search&track=sph&uuid=cb4c7165-b088-46ff-9393-1d7abcce3fe8">
+            <a target="_blank" href="https://br.freepik.com/fotos-gratis/casal-adoravel-tem-um-abraco-caloroso_13759431.htm#query=casal&position=0&from_view=search&track=sph&uuid=cb4c7165-b088-46ff-9393-1d7abcce3fe8">
               Imagem de wayhomestudio
             </a>{" "}
             no Freepik
@@ -68,7 +68,7 @@ export default function SectionMarquee() {
           />
           <p className="text-xs text-slate-400">
             Imagem de{" "}
-            <a href="https://br.freepik.com/fotos-gratis/pares-bonitos-que-passam-tempo-juntos_66194829.htm#query=casal&position=2&from_view=search&track=sph&uuid=cb4c7165-b088-46ff-9393-1d7abcce3fe8">
+            <a target="_blank" href="https://br.freepik.com/fotos-gratis/pares-bonitos-que-passam-tempo-juntos_66194829.htm#query=casal&position=2&from_view=search&track=sph&uuid=cb4c7165-b088-46ff-9393-1d7abcce3fe8">
               Freepik
             </a>
           </p>
@@ -83,7 +83,7 @@ export default function SectionMarquee() {
           />
           <p className="text-xs text-slate-400">
             Imagem de{" "}
-            <a href="https://br.freepik.com/fotos-gratis/vista-traseira-casal-sentado-na-natureza_72566482.htm#query=casal&position=40&from_view=search&track=sph&uuid=cb4c7165-b088-46ff-9393-1d7abcce3fe8">
+            <a target="_blank" href="https://br.freepik.com/fotos-gratis/vista-traseira-casal-sentado-na-natureza_72566482.htm#query=casal&position=40&from_view=search&track=sph&uuid=cb4c7165-b088-46ff-9393-1d7abcce3fe8">
               Freepik
             </a>
           </p>
