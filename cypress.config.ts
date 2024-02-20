1e2249b40c10bcffa75fc132e38a6104236f0b75
Anna Nayara Lira <anna_nayara@hotmail.com>
2024-02-20 16:43:25 -0300

    feat: instalação de lib de testes cypress

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+// rodar testes: npx cypress open
+
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3000',
+  },
+});
